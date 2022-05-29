95476e62e5594256fbc563479389305507bded07
baykalcelik <baykalcelik2022@gmail.com>
2022-05-29 17:21:17 +0300

    teneffus arası

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('#buton1').addEventListener('click',function(event){
+//buton 1 tıklandığında çalışacak kod
+alert('buton1 tıklandı');
+});
+
+// buton1 border rengi değişsin
+document.querySelector('#buton1').getElementsByClassName.border = "2px solid black";
